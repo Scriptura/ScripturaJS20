c1cd82807c403201d6eb8efcf91237b2503c4cdd
Olivier Chavarin <memoire1@gmail.com>
2020-05-28 11:06:57 +0200

    Update routesDispatcher.js

--- controllers/routesDispatcher.js
+++ controllers/routesDispatcher.js
@@ -3,37 +3,37 @@
 const express = require('express'),
       router = express.Router()
 
-      router.use(
-        '/',
-        require('./index')
-      )
-      router.use(
-        '/',
-        require('./article')
-      )
-      router.use(
-        '/',
-        require('./person')
-      )
-      router.use(
-        '/',
-        require('./user')
-      )
-      router.use(
-        '/',
-        require('./place')
-      )
-      router.use(
-        '/',
-        require('./login')
-      )
-      router.use(
-        '/',
-        require('./os')
-      )
-      router.use(
-        '/',
-        require('./styleGuide')
-      )
+router.use(
+  '/',
+  require('./index')
+)
+router.use(
+  '/',
+  require('./article')
+)
+router.use(
+  '/',
+  require('./person')
+)
+router.use(
+  '/',
+  require('./user')
+)
+router.use(
+  '/',
+  require('./place')
+)
+router.use(
+  '/',
+  require('./login')
+)
+router.use(
+  '/',
+  require('./os')
+)
+  router.use(
+  '/',
+  require('./styleGuide')
+)
 
 module.exports = router
