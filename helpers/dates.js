feb30d2d713905cf93d811768b6ca7407e4af7b3
Olivier Chavarin <memoire1@gmail.com>
2020-09-20 06:24:01 +0200

    All packages updated

--- helpers/dates.js
+++ helpers/dates.js
@@ -2,14 +2,16 @@
 
 const { DateTime } = require('luxon')
 
+// @see https://moment.github.io/luxon/docs/manual/formatting
 // @see https://moment.github.io/luxon/demo/global.html
 
 const dateFormat = (date) => {
-  return DateTime.fromJSDate(date).setLocale('fr').toFormat('dd/MM/yyyy')
+  return DateTime.fromJSDate(date).toFormat('dd/MM/yyyy')
 }
 
 const dateTimeFormat = (date) => {
-  return DateTime.fromJSDate(date).setLocale('fr').toFormat('dd/MM/yyyy HH:mm')
+  return DateTime.fromJSDate(date).toFormat('dd/MM/yyyy HH:mm')
+  //return DateTime.fromJSDate(date).setLocale('fr').toLocaleString(DateTime.DATE_FULL)
 }
 
 module.exports = {
