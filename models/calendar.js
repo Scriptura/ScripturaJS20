60e194bcdd4779861c01afcd82fcd89d987ce996
Olivier Chavarin <memoire1@gmail.com>
2020-12-14 01:28:16 +0100

    Liturgical calendar revised

--- models/calendar.js
+++ models/calendar.js
@@ -5,7 +5,7 @@ const vv = require('../settings/variables'),
       { moonPhase } = require('../helpers/astronomy'),
       { DateTime } = require('luxon'),
       currentDate = DateTime.local(),
-      //currentDate = DateTime.fromFormat('24062018', 'ddMMyyyy'), // @note For test // '28042019'
+      //currentDate = DateTime.fromFormat('01062020', 'ddMMyyyy'), // @note For test // '28042019'
       lc = liturgicalCalendar(currentDate)
 
 const getCalendar = {
