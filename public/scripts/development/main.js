b836337946b768a5c36668dea40735315b874711
Olivier Chavarin <memoire1@gmail.com>
2021-07-15 11:38:59 +0200

    Players revised

--- public/scripts/development/main.js
+++ public/scripts/development/main.js
@@ -402,7 +402,6 @@ const imageFocus = (() => {
   const wrapClone = el => {
     const wrapper = document.createElement('div')
     wrapper.classList.add('focus-off')
-    console.log(wrapper)
     el.parentNode.insertBefore(wrapper, el)
     wrapper.appendChild(el)
     addButtonShrink()
@@ -494,7 +493,6 @@ const lineMarks = (el => {
     const text = document.createTextNode(i)
     a.appendChild(text)
     a.classList.add('line-mark')
-    console.log(a)
     el.appendChild(a)
   }
   let i = 0
@@ -503,6 +501,34 @@ const lineMarks = (el => {
     //if (i % 5 === 0) {}
     lineMarksAdd(el)
   }
+})()
+
+
+// -----------------------------------------------------------------------------
+// @section     Media players
+// @description Lecteurs audio utilisant la spécification HTMLMediaElement
+// -----------------------------------------------------------------------------
+
+const secondsToTime = e => { // @see https://stackoverflow.com/questions/3733227/javascript-seconds-to-minutes-and-seconds
+  let hh = Math.floor(e / 3600).toString().padStart(2, '0'),
+      mm = Math.floor(e % 3600 / 60).toString().padStart(2, '0'),
+      ss = Math.floor(e % 60).toString().padStart(2, '0')
+  if (hh == '00') hh = null // Si pas d'heures, alors info sur les heures escamotée
+  const result = [hh, mm, ss].filter(Boolean).join(':')
+  return result
+}
+
+const audioPlayer = (() => {
+  const audios = document.querySelectorAll('.audio')
+  const addAudioPlayer = (() => {
+    let i = 0
+    for (const audio of audios) {
+      i++
+      const player = `<div class="audio-player"><button class="audio-play-pause"><svg xmlns="http://www.w3.org/2000/svg" width="1024" height="1024" viewBox="0 0 1024 1024"><path d="M204.524 102.03L819.48 512 204.523 921.97z"/></svg></button><output class="audio-player-current-time">0:00</output><div class="progress"></div><output class="audio-player-duration">0:00</output><div><button onclick="document.getElementsByClassName('player${i}')[0].volume += 0.1">+</button><button onclick="document.getElementsByClassName('player${i}')[0].volume -= 0.1">-</button></div></div>`
+      audio.classList.add('player' + i)
+      audio.insertAdjacentHTML('afterend', player)
+    }
+  })()
 })()
 
 
